90ef4f5aacc3f153c766ce581d988be6ba450984
Ilber Pineda <ilberpineda@gmail.com>
2020-12-13 19:36:36 -0800

    added comments for understanding

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const black = document.querySelector('#black')
 function makeGrid(){
     for (let i = 0; i < 256; i++){ //tells it to run the loop until number of rows required is met
         const div = document.createElement('div');  //creates a div element
-        div.classList.add('cell');
-        container.appendChild(div)
-        div.addEventListener('mouseover', function(event){
+        div.classList.add('cell'); //creates cell class child to div
+        container.appendChild(div) //appends the div child to parent container
+        div.addEventListener('mouseover', function(event){ // creates an event listener to change colors in cell to black
             event.target.style.backgroundColor = 'black';
             
             setTimeout(function(){
@@ -21,7 +21,7 @@ function makeGrid(){
     }
 }
 
-function rainbowColor(){
+function rainbowColor(){ //function to set a random color to be chosen
     let letters = '0123456789ABCDEF'
     let colors = '#'
     for(let i = 0; i < 6; i++){
@@ -31,48 +31,48 @@ function rainbowColor(){
     
 }
 
-rgb.addEventListener('click', function(){
+rgb.addEventListener('click', function(){ //function that adds event listener to rainbow button when clicked
     let value = document.getElementById('slide').value;
     let cell = container.children;
     for(let i = 0; i < value*value; i++){
-        cell[i].addEventListener('mouseover', function(event){
+        cell[i].addEventListener('mouseover', function(event){ //adds eventlistener that will apply the random color to be shown when going over cells
             event.target.style.backgroundColor = rainbowColor();
         })
     }
 });
 
-black.addEventListener('click', function(){
+black.addEventListener('click', function(){ //function that adds event listener to black button when clicked
     let value = document.getElementById('slide').value;
     let cell = container.children;
     for(let i = 0; i < value*value; i++){
-        cell[i].addEventListener('mouseover', function(){
+        cell[i].addEventListener('mouseover', function(){ //adds event listener to mouseover to make it black when button is clicked
             event.target.style.backgroundColor = 'black'
         })
     }
 });
 
-function removeAllChildNodes(parent){
+function removeAllChildNodes(parent){ //function for removing child nodes from the parent eg creating a new grid
     while(parent.firstChild){
         parent.removeChild(parent.firstChild)
     }
 }
 
-slider.addEventListener('input', function(){
+slider.addEventListener('input', function(){ //event listener for changing the value of the grid size within the container
     let value = document.getElementById('slide').value;
     size.textContent = value;
-    removeAllChildNodes(container);
+    removeAllChildNodes(container); //uses the remove child function for the children of the container
     container.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 3fr);`);
     for(let i = 0; i < value*value; i++) {
-        const div = document.createElement('div');
+        const div = document.createElement('div'); //creates new size of container grid
         div.classList.add('cell');
         container.appendChild(div)
-        div.addEventListener('mouseover', function(event){
+        div.addEventListener('mouseover', function(event){ //adds black mouse over when new grid is created
             event.target.style.backgroundColor = 'black'
         })
     }
 })
 
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function(){ //adds event listener to grid button and when clicked creates new 'clean' grid
     let value = document.getElementById('slide').value;
     let cell = container.children;
     for(let i = 0; i < value*value; i++) {
@@ -83,6 +83,9 @@ reset.addEventListener('click', function(){
 
 
 makeGrid()
+
+//all old code that didnt work
+
 // function changeColor(event){
 //     let div = event.target.document.querySelector('#container');
 //     return div.style.backgroundColor = 'black';
